fd50bc5c9f94d859f87079cf7acfe59ae778f39c
Rahul Lap <rahulbushi69@gmail.com>
2024-10-24 18:27:42 +0530

    Remove unnecessary SVG and ESLint files

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,7 @@
+import { getAuth } from 'firebase/auth';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC6Jf_gYDPymLZ4S1PC_aXzBbW4oVbAIEc",
   authDomain: "url-short-62eff.firebaseapp.com",
@@ -18,5 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
